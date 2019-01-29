46d441e493a8c5c2ad4b8d0c3abf41c126a1f68d
lautaskriptaaja <47041005+lautaskriptaaja@users.noreply.github.com>
2019-01-29 06:08:21 +0200

    Update spamminesto.user.js

--- spamminesto.user.js
+++ spamminesto.user.js
@@ -4,7 +4,7 @@
 // @exclude *://ylilauta.org/hiddenthreads
 // @require https://github.com/lautaskriptaaja/Ylis-spamfilter/raw/master/blacklist.txt
 // @require https://github.com/lautaskriptaaja/Ylis-spamfilter/raw/master/runsafely.user.js
-// @version 0.55
+// @version 0.56
 // @locale en
 // @description Piilottaa langat ja vastaukset automaattisesti joissa on jokin mustalistattu sana tai luokitellaan spämmiksi
 // ==/UserScript==
@@ -21,6 +21,7 @@ let foreign_threshold=0.1; //muuttujaksi asetuksiin, jos tekstissä on enemmän 
 let enable_flood_restriction=true; //muuttujaksi asetuksiin, melko tiukka floodin esto -menetelmä, optimointivaraa
 let detect_flood_minutes=5; //kuinka pitkältä ajalta hakee floodia
 let hide_same_videos=true; //muuttujaksi...
+let hide_wiki_texts=true;
 let LocalStorage;
 let emojiPattern=/(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|[\ud83c[\ude50\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
 
@@ -171,6 +172,17 @@ function preventSameVideo(post) {
 
 }
 
+function detectWiki(text) {
+  if (!hide_wiki_texts)
+    return false;
+  let regex=/(\[[0-9]\])/g;
+  let list = text.match(regex);
+  if (list!=null && list.length>=2)
+    return true;
+  else
+    return false;
+}
+
 //tunnistetaan mahdollinen floodi
 function detectFlood(post) {
   if (!enable_flood_restriction)
@@ -257,6 +269,8 @@ function removeItem(key, style) {
 function hideLoop(post, style) {
   const text = getPostText(post);
   if (hideBlackList(text))
+    hidePost(post, style)
+  else if (detectWiki(text))
     hidePost(post, style)
   else if (preventSameVideo(post))
     hidePost(post, style)
